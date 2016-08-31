8562c49ae7f2e7b90d9278c0920a6f23048c5b75
Arturo <artw23@gmail.com>
2016-08-30 21:13:00 -0500

    Commit inicial

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * File:   main.c
+ * Author: Rt
+ *
+ * Created on August 30, 2016, 9:08 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=cansat_2016.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/cansat_2016.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=cansat2016.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/cansat2016.x.tar
